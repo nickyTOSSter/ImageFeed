9eefa080e88dc1961ea4a9d159f48ac6b19f3eba
Nikita Chagochkin <nick.chagochkin@yandex.ru>
2023-03-12 14:23:51 +0300

    sprint 10 to review

---
+++ ImageFeed/Auth/OAuth2TokenStorage.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+class OAuth2TokenStorage {
+    var token: String? {
+        get {
+            UserDefaults.standard.string(forKey: "token")
+        }
+        
+        set {
+            UserDefaults.standard.setValue(newValue, forKey: "token")
+        }
+    }
+    
+}
---
+++ ImageFeed/Auth/OAuthTokenResponseBody.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+struct OAuthTokenResponseBody: Decodable {
+    let accessToken: String
+    
+    enum CodingKeys: String, CodingKey {
+       case accessToken = "access_token"
+    }
+}
